f6e6996c742f61b8ae0fc1730551c1b2e4ff767d
Ron Turetzky <turetzkyron@gmail.com>
2025-05-20 15:51:43 -0400

    feat: adding sig verification logic

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,16 +1,60 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
-import {BLSSigCheckOperatorStateRetriever} from "lib/eigenlayer-middleware/src/unaudited/BLSSigCheckOperatorStateRetriever.sol";
+
+import {BLSSigCheckOperatorStateRetriever} from
+    "lib/eigenlayer-middleware/src/unaudited/BLSSigCheckOperatorStateRetriever.sol";
 import {BLSSignatureChecker} from "lib/eigenlayer-middleware/src/BLSSignatureChecker.sol";
 import {ISlashingRegistryCoordinator} from "lib/eigenlayer-middleware/src/interfaces/ISlashingRegistryCoordinator.sol";
+
 contract Counter is BLSSigCheckOperatorStateRetriever, BLSSignatureChecker {
+    /// @title Counter
+    /// @notice A simple counter contract that implements BLS signature checking
+    /// @dev Inherits from BLSSigCheckOperatorStateRetriever and BLSSignatureChecker
+    /// @param _registryCoordinator The address of the registry coordinator contract
+
+    /// @notice The current number
     uint256 public number;
+    /// @notice The block number stale measure
+    uint32 public BLOCK_STALE_MEASURE = 300;
+    /// @notice The quorum threshold
+    uint256 public QUORUM_THRESHOLD = 66; // 66% of the quorum
+    /// @notice The threshold denominator
+    uint256 public THRESHOLD_DENOMINATOR = 100;
 
-    constructor(
-        ISlashingRegistryCoordinator _registryCoordinator
-    ) BLSSignatureChecker(_registryCoordinator) {}
+    /// @notice The error for future block number
+    error FutureBlockNumber();
+    /// @notice The error for stale block number
+    error StaleBlockNumber();
+    /// @notice The error for invalid hash
+    error InvalidHash();
+    /// @notice The error for insufficient quorum threshold
+    error InsufficientQuorumThreshold();
 
-    function increment() public {
+    constructor(ISlashingRegistryCoordinator _registryCoordinator) BLSSignatureChecker(_registryCoordinator) {}
+
+    function increment(
+        bytes32 msgHash,
+        bytes calldata quorumNumbers,
+        uint32 referenceBlockNumber,
+        NonSignerStakesAndSignature memory params
+    ) public {
+        require(referenceBlockNumber < block.number, FutureBlockNumber());
+        require((referenceBlockNumber + BLOCK_STALE_MEASURE) >= uint32(block.number), StaleBlockNumber());
+
+        bytes32 expectedHash = sha256(abi.encode(number));
+        require(msgHash == expectedHash, InvalidHash());
+         (QuorumStakeTotals memory stakeTotals,) =
+        checkSignatures(msgHash, quorumNumbers, referenceBlockNumber, params);
+
+        // Check that signatories own at least 66% of each quorum
+        for (uint256 i = 0; i < quorumNumbers.length; i++) {
+            require(
+                stakeTotals.signedStakeForQuorum[i] * THRESHOLD_DENOMINATOR
+                    >= stakeTotals.totalStakeForQuorum[i] * QUORUM_THRESHOLD,
+                InsufficientQuorumThreshold()
+            );
+        }
+
         number++;
     }
 }
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -4,16 +4,12 @@ pragma solidity ^0.8.13;
 import {Test, console} from "forge-std/Test.sol";
 import {Counter} from "../src/Counter.sol";
 import {ISlashingRegistryCoordinator} from "lib/eigenlayer-middleware/src/interfaces/ISlashingRegistryCoordinator.sol";
+
 contract CounterTest is Test {
     Counter public counter;
 
     function setUp() public {
         address registryCoordinator = vm.envAddress("REGISTRY_COORDINATOR_ADDRESS");
         counter = new Counter(ISlashingRegistryCoordinator(registryCoordinator));
-    }
-
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
     }
 }
